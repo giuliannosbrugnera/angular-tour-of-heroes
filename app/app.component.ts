e9727e7ba4a405338dd2e4aa886498a1da1119a6
Giulianno Sbrugnera <giulianno@opus-software.com.br>
2016-09-30 14:19:13 -0300

    Add click handler and exposing the selected hero

--- app/app.component.ts
+++ app/app.component.ts
@@ -24,10 +24,16 @@ const HEROES: Hero[] = [
         <h1>{{title}}</h1>
         <h2>My Heroes</h2>
         <ul class="heroes">
-          <li *ngFor="let hero of heroes">
-            <span class="badge">{{hero.id}}</span> {{hero.name}}
-          </li>
+            <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+                <span class="badge">{{hero.id}}</span> {{hero.name}}
+            </li>
         </ul>
+        <h2>{{selectedHero.name}} details!</h2>
+        <div><label>id: </label>{{selectedHero.id}}</div>
+        <div>
+            <label>name: </label>
+            <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+        </div>
         `,
     styles: [`
         .selected {
@@ -83,4 +89,9 @@ const HEROES: Hero[] = [
 export class AppComponent {
     title = 'Tour of Heroes';
     heroes = HEROES;
+    selectedHero = Hero;
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 }
