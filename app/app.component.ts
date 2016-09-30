aeb8817619ef4a639f62925d5c18ad1e189483db
Giulianno Sbrugnera <giulianno@opus-software.com.br>
2016-09-30 14:01:45 -0300

    Listing heroes with ngFor

--- app/app.component.ts
+++ app/app.component.ts
@@ -24,16 +24,10 @@ const HEROES: Hero[] = [
         <h1>{{title}}</h1>
         <h2>My Heroes</h2>
         <ul class="heroes">
-          <li>
-            <!-- each hero goes here -->
+          <li *ngFor="let hero of heroes">
+            <span class="badge">{{hero.id}}</span> {{hero.name}}
           </li>
         </ul>
-        <h2>{{hero.name}} details!</h2>
-        <div><label>id: </label>{{hero.id}}</div>
-        <div>
-            <label>name: </label>
-            <input [(ngModel)]="hero.name" placeholder="name">
-        </div>
         `
 })
 export class AppComponent {
