9ca03828cc9bf64dd36e3e819dd05e5ad034b141
Giulianno Sbrugnera <giulianno@opus-software.com.br>
2016-09-30 14:50:41 -0300

    Hero detail component

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -3,6 +3,16 @@ import {Component, Input} from '@angular/core';
 @Component({
     /* selector name that identifies this component's element */
     selector: 'my-hero-detail',
+    template: `
+        <div *ngIf="selectedHero">
+            <h2>{{selectedHero.name}} details!</h2>
+            <div><label>id: </label>{{selectedHero.id}}</div>
+            <div>
+                <label>name: </label>
+                <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+            </div>
+        </div>
+        `
 })
 
 /* export the class to make it available to other components */
