1897d7e86d45b8987d4c752a471932c94ba4f768
Giulianno Sbrugnera <giulianno@opus-software.com.br>
2016-09-30 14:48:28 -0300

    Separating the Hero Detail Component

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+    /* selector name that identifies this component's element */
+    selector: 'my-hero-detail',
+})
+
+/* export the class to make it available to other components */
+export class HeroDetailComponent {
+}
