82be2120cfc71b0f2a6b74181e277bf6fb63d291
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2016-10-01 12:22:20 -0300

    Move content out of the component file

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,8 +1,9 @@
-import {Component} from '@angular/core';
-import {Component, OnInit} from '@angular/core';
+import {Component}          from '@angular/core';
+import {Component, OnInit}  from '@angular/core';
+import {Router}             from '@angular/router';
 
-import {Hero} from './hero';
-import {HeroService} from './hero.service';
+import {Hero}               from './hero';
+import {HeroService}        from './hero.service';
 
 
 @Component({
@@ -15,7 +16,8 @@ export class DashboardComponent implements OnInit {
 
     heroes: Hero[] = [];
 
-    constructor(private heroService: HeroService) {
+    constructor(private router: Router,
+                private heroService: HeroService) {
     }
 
     ngOnInit(): void {
@@ -23,6 +25,8 @@ export class DashboardComponent implements OnInit {
             .then(heroes => this.heroes = heroes.slice(1, 5));
     }
 
-    gotoDetail(hero: Hero): void { /* not implemented yet */
+    gotoDetail(hero: Hero): void {
+        let link = ['/detail', hero.id];
+        this.router.navigate(link);
     }
 }
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,14 +1,14 @@
 // Keep the Input import for now, we'll remove it later:
-import {Component, Input, OnInit}   from '@angular/core';
-import {ActivatedRoute, Params}     from '@angular/router';
-import {Location}                   from '@angular/common';
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute, Params}   from '@angular/router';
+import {Location}                 from '@angular/common';
 
-import {Hero}                       from './hero';
-import {HeroService}                from './hero.service';
+import {Hero}                     from './hero';
+import {HeroService}              from './hero.service';
 
 @Component({
     selector: 'my-hero-detail',
-    templateUrl: 'hero-detail.component.html'
+    templateUrl: './app/hero-detail.component.html'
 })
 
 export class HeroDetailComponent implements OnInit {
