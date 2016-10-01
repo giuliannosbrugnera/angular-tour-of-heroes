015bffbcb54582148337b1e94eec6c6b9c38adf7
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2016-10-01 13:29:15 -0300

    Heroes and HTTP

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,16 +1,32 @@
-import {Injectable} from '@angular/core';
+import {Injectable}       from '@angular/core';
+import {Headers, Http}    from '@angular/http';
 
-import {Hero}       from './hero';
-import {HEROES}     from './mock-heroes';
+import 'rxjs/add/operator/toPromise';
+
+import {Hero}             from './hero';
+
 
 @Injectable()
 export class HeroService {
+    private heroesUrl = 'app/heroes';  // URL to web api
+
+    constructor(private http: Http) {
+    }
+
     getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data as Hero[])
+            .catch(this.handleError);
     }
 
     getHero(id: number): Promise<Hero> {
         return this.getHeroes()
             .then(heroes => heroes.find(hero => hero.id === id));
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
     }
 }
