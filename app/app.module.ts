08ac18f98b263d9b132b9520e925d0186e9afbb1
giuliannosbrugnera <giulianno.sbrugnera@gmail.com>
2016-10-01 13:09:04 -0300

    Register for HTTP services

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,27 +1,30 @@
-import {NgModule}               from '@angular/core';
-import {BrowserModule}          from '@angular/platform-browser';
-import {FormsModule}            from '@angular/forms';
-
-import {AppComponent}           from './app.component';
-import {HeroDetailComponent}    from './hero-detail.component';
-import {HeroesComponent}        from './heroes.component';
-import {HeroService}            from './hero.service';
-import {routing}                from './app.routing';
-import {DashboardComponent}     from './dashboard.component';
+import {NgModule}             from '@angular/core';
+import {BrowserModule}        from '@angular/platform-browser';
+import {FormsModule}          from '@angular/forms';
+import {HttpModule}           from '@angular/http';
+import {AppComponent}         from './app.component';
+import {DashboardComponent}   from './dashboard.component';
+import {HeroesComponent}      from './heroes.component';
+import {HeroDetailComponent}  from './hero-detail.component';
+import {HeroService}          from './hero.service';
+import {routing}              from './app.routing';
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
+        HttpModule,
         routing
     ],
     declarations: [
         AppComponent,
+        DashboardComponent,
         HeroDetailComponent,
         HeroesComponent,
-        DashboardComponent
     ],
-    providers: [HeroService],
+    providers: [
+        HeroService,
+    ],
     bootstrap: [AppComponent]
 })
 
